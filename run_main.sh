6ab25efb021c56e2ef37c41009decbd10b10b8d9
b381361 <leon@tropos.de>
2024-10-25 09:57:39 +0200

    New description file (README) and sbatch running script

---
+++ run_main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J mypyjb          # Specify job name
+#SBATCH -p compute         # Use partition shared
+#SBATCH -N 1               # Specify number of nodes (1 for serial applications!)
+#SBATCH -n 1               # Specify max. number of tasks to be invoked
+#SBATCH -t 01:00:00        # Set a limit on the total run time
+#SBATCH -A bb1005          # Charge resources on this project account
+#SBATCH -o myjob.o%j       # File name for standard and error output
+#SBATCH --mem-per-cpu=1920M
+
+set -e
+
+# Environment:
+module load python3
+ulimit -s # 204800
+ulimit -c 0
+#export OMP_STACKSIZE=900M
+
+echo "Start python script execution at $(date)"
+
+python -u main.py
+
+
